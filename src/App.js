afa75a8d25deac25248ebaa5360a81e35a19fd68
kayemba <kayembejonathan5@gmail.com>
2023-05-07 17:17:05 +0200

    Add the navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Calculator from './Components/calculator';
+import Navigation from './Components/navigation';
+import Home from './Components/home';
 import Quote from './Components/Quote';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quote />
-    </div>
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
---
+++ src/Components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const text = 'In this project, I will create a full website for the Math magicians app, consisting of several pages,';
+
+const Home = () => (
+  <div className="nav">
+    <h4>Welcome Website</h4>
+    <p>{text}</p>
+  </div>
+);
+
+export default Home;
